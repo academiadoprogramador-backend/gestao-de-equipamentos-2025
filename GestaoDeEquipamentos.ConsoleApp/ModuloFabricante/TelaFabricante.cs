dce0494f044bfc21354ae05c338e180bc3b324e0
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-07-17 20:39:24 -0300

    Implementado ChamadoController completo

--- GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/TelaFabricante.cs
+++ GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/TelaFabricante.cs
@@ -1,7 +1,6 @@
 ﻿using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
 using GestaoDeEquipamentos.Dominio.ModuloFabricante;
 using GestaoDeEquipamentos.Infraestrutura.Arquivos.ModuloFabricante;
-using GestaoDeEquipamentos.Infraestrutura.ModuloFabricante;
 
 namespace GestaoDeEquipamentos.ConsoleApp.ModuloFabricante;
 
